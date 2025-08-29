f6f7aa6ab5a1fb7a9aeaa9d2f0c8ad6dc0a1f32b
NikeshTechM <nikesh.sar@techmahindra.com>
2025-08-29 12:23:49 +0530

    Update seat_adjustment.cpp

--- seat_adjustment.cpp
+++ seat_adjustment.cpp
@@ -142,8 +142,8 @@ void adjust_seat(const std::string& name, ordered_json current, ordered_json tar
                 send_to_nxp(json_str);
             }).detach();
 
-           // usleep(50 * 1000);  // 50ms pacing for Android
-            sleep(1);
+           usleep(50 * 1000);  // 50ms pacing for Android
+           //sleep(1);
         }
     }
 
